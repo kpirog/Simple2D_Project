bba3aa3dd545d58d0e2de6bdbd27b1c18851c8d5
Kamil Piróg <62013599+kpirog@users.noreply.github.com>
2022-07-21 12:58:15 +0200

    NavMesh2D addons

--- Assets/_Scripts/Enemy/EnemyStateMachine.cs
+++ Assets/_Scripts/Enemy/EnemyStateMachine.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyStateMachine : MonoBehaviour
 {
     private EnemyBaseState currentState;
 
+    private NavMeshAgent agent;
+    private PlayerHealth player;
+
+    [HideInInspector] public Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        player = FindObjectOfType<PlayerHealth>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
     private void Start()
     {
         SwitchState(new EnemyIdleState(this));
