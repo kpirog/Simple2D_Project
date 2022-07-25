cfcc2b351a5d9c245bcad7408dfe47411b4807b0
Kamil Piróg <62013599+kpirog@users.noreply.github.com>
2022-07-25 17:47:47 +0200

    Rounds data implementation

---
+++ Assets/_Scripts/Rounds/GameRoundData.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewGameRoundData", menuName = "GameRounds/Round", order = 0)]
+public class GameRoundData : ScriptableObject
+{
+    [Header("Spawning settings")]
+    [SerializeField][Range(1, 4)] private int activeSpawnersCount;
+    [SerializeField] private float timeToFirstSpawn;
+    [SerializeField] private float spawnRate;
+    [SerializeField] private float spawnDelay;
+
+    [Header("Round Goal settings")]
+    [SerializeField] private int mushroomsToKill;
+    [SerializeField] private int goblinsToKill;
+    [SerializeField] private int goldToCollect;
+
+    public int ActiveSpawnersCount => activeSpawnersCount;
+    public float TimeToFirstSpawn => timeToFirstSpawn;
+    public float SpawnRate => spawnRate;
+    public float SpawnDelay => spawnDelay;
+    public int MushroomsToKill => mushroomsToKill;
+    public int GoblinsToKill => goblinsToKill;
+    public int GoldToCollect => goldToCollect;  
+
+    private void OnValidate()
+    {
+        if (timeToFirstSpawn <= 0)
+            timeToFirstSpawn = 0.1f;
+
+        if (spawnRate <= 0)
+            spawnRate = 1f;
+
+        if (spawnDelay < 1)
+            spawnDelay = 1;
+    }
+}
