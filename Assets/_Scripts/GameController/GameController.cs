da7bc9f1f3f24ceac782d876f56b311533057431
Kamil Piróg <62013599+kpirog@users.noreply.github.com>
2022-07-25 18:02:13 +0200

    Import UI Assets and fonts

--- Assets/_Scripts/GameController/GameController.cs
+++ Assets/_Scripts/GameController/GameController.cs
@@ -1,8 +1,13 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
     public static GameController Instance;
+
+    [SerializeField] private List<GameRoundData> roundDataList;
+
+    private int currentRound = 0;
 
     private void Awake()
     {
@@ -13,5 +18,14 @@ public class GameController : MonoBehaviour
         }
 
         Instance = this;
+    }
+
+    private void Start()
+    {
+        
+    }
+    private void LoadCurrentRound()
+    {
+
     }
 }
